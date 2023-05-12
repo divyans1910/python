e361da23765fde9dda669c0377f3c1e8bb6836ab
divyans1910 <133007398+divyans1910@users.noreply.github.com>
2023-05-12 10:19:43 +0530

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,76 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+               'u', 'v', 'w', 'x', 'y', 'z']
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+#Input
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#Encrypt function
+def encrypt(text,shift):
+  encoded=""
+  for letter in text:
+    pos=alphabet.index(letter)
+    newpos=pos+shift
+    newletter=alphabet[newpos]
+    encoded+=newletter
+  print("The encoded text is: ",encoded)
+
+#Decrypt function
+def decrypt(text,shift):
+  decoded=""
+  for letter in text:
+    pos=alphabet.index(letter)
+    newpos=pos-shift
+    newletter=alphabet[newpos]
+    decoded+=newletter
+  print("The decoded text is: ",decoded)
+
+#Caesar function
+def caesar(text,shift,direction):
+  if direction=="encode":
+    encrypt(text,shift)
+  elif direction=="decode":
+    decrypt(text,shift)
+  else:
+    print("Sorry, Try again")
+
+#Findiing encoded and decoded
+caesar(text,shift,direction)
+
+#Restart
+shdcontinue=True
+while shdcontinue:
+  print(logo)
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift=shift % 25
+  caesar(text,shift,direction)
+
+
+
+
+   
